43ae41ee3d247f888c7001e5ecf4787cb34e75fa
Andrei Gunta <andreigunta@yahoo.com>
2025-07-31 21:01:39 +0300

    Adding step defs

---
+++ src/test/java/step_definitions/ContactUs_steps.java
@@ -0,0 +1,38 @@
+package step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ContactUs_steps {
+
+    @When("I type a first name")
+    public void iTypeAFirstName() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I type a last name")
+    public void iTypeALastName() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I enter an email address")
+    public void iEnterAnEmailAddress() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I type a comment")
+    public void iTypeAComment() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click on the submit button")
+    public void iClickOnTheSubmitButton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should be presented with a successful contact us submission message")
+    public void iShouldBePresentedWithASuccessfulContacUsSubmissionMessage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/java/step_definitions/Homepage_steps.java
@@ -0,0 +1,18 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class Homepage_steps {
+
+    @Given("I navigate to the webdriveruniversity homepage")
+    public void iNavigateToTheWebdriverUniversityHomepage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click on the contact us button")
+    public void iClickOnTheContactUsButton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
