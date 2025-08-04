c834947847f1e8821bbbb60a0ea9c46947735122
Andrei Gunta <andreigunta@yahoo.com>
2025-08-04 12:24:07 +0300

    Added Background keyword

--- src/test/java/step_definitions/ContactUs_steps.java
+++ src/test/java/step_definitions/ContactUs_steps.java
@@ -8,6 +8,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.datafaker.Faker;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,8 +16,8 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 import static org.testng.Assert.assertTrue;
 
 public class ContactUs_steps {
-        public BrowserManager browserManager;
-        private final Faker faker = new Faker();
+    public BrowserManager browserManager;
+    private final Faker faker = new Faker();
 
     public ContactUs_steps(BrowserManager browserManager) {
         this.browserManager = browserManager;
@@ -26,18 +27,22 @@ public class ContactUs_steps {
     public void iTypeAFirstName() {
         browserManager.page.getByPlaceholder("First Name").fill("Joe");
     }
+
     @And("I type a last name")
     public void iTypeALastName() {
         browserManager.page.getByPlaceholder("Last Name").fill("Doe");
     }
+
     @When("I enter an email address")
     public void iEnterAnEmailAddress() {
         browserManager.page.getByPlaceholder("Email Address").fill("agtest1@yopmail.com");
     }
+
     @When("I type a comment")
     public void iTypeAComment() {
         browserManager.page.getByPlaceholder("Comments").fill("Hello World!");
     }
+
     @When("I click on the submit button")
     public void iClickOnTheSubmitButton() {
         // custom wait
@@ -48,6 +53,7 @@ public class ContactUs_steps {
         browserManager.page.click("input[value='SUBMIT']");
 //        browserManager.page.getByText("SUBMIT").click();
     }
+
     @And("I type a specific first name {string}")
     public void iTypeASpecificFirstName(String firstName) {
         browserManager.page.getByPlaceholder("First Name").fill(firstName);
@@ -104,5 +110,40 @@ public class ContactUs_steps {
         Pattern pattern = Pattern.compile("Error: (all fields are required|Invalid email address)");
         Matcher matcher = pattern.matcher(bodyText);
         assertTrue(matcher.find(), "Text does not match text: '" + bodyText + "'");
+    }
+
+    @And("I type a first name {word} and a last name {word}")
+    public void iTypeAFirstNameFirstNameAndALastNameLastName(String firstName, String lastName) {
+        browserManager.page.getByPlaceholder("First Name").fill(firstName);
+        browserManager.page.getByPlaceholder("Last Name").fill(lastName);
+    }
+
+    @And("I type a email address {string} and a comment {string}")
+    public void iTypeAEmailAddressEmailAddressAndACommentComment(String emailAddress, String comment) {
+        browserManager.page.getByPlaceholder("Email Address").fill(emailAddress);
+        browserManager.page.getByPlaceholder("Comments").fill(comment);
+    }
+
+    @Then("I should be presented with header text {string}")
+    public void iShouldBePresentedWithHeaderTextMessage(String message) {
+        //Wait for the target element
+        //h1 | body
+        browserManager.page.waitForSelector("//h1 | //body"); //dynamic selector h1 OR body
+        //Get all elements inner text
+        List<String> texts = browserManager.page.locator("//h1 | //body").allInnerTexts();
+        //Variable to store the found text
+        String foundText = "";
+        //Check if any of the text include the expected message
+        boolean found = false;
+        for (String text : texts) {
+            if (text.contains(message)) {
+                foundText = text;
+                found = true;
+                break;
+            } else {
+                foundText = text;
+            }
+        }
+        assertTrue(found, "Expected message: " + foundText + ", to be equal to: " + message);
     }
 }
--- src/test/resources/features/ContactUs.feature
+++ src/test/resources/features/ContactUs.feature
@@ -1,9 +1,11 @@
 Feature: WebdriverUniversity.com - Contact Us Page
 
-  Scenario: Valid Contact Us Form Submission
+  Background: Pre Conditions
     Given I navigate to the webdriveruniversity homepage
     When I click on the contact us button
-    And I type a first name
+
+  Scenario: Valid Contact Us Form Submission
+    When I type a first name
     And I type a last name
     And I enter an email address
     And I type a comment
@@ -11,18 +13,14 @@ Feature: WebdriverUniversity.com - Contact Us Page
     Then I should be presented with a successful contact us submission message
 
   Scenario: Invalid Contact Us Form Submission
-    Given I navigate to the webdriveruniversity homepage
-    When I click on the contact us button
-    And I type a last name
+    When I type a last name
     And I enter an email address
     And I type a comment
     And I click on the submit button
     Then I should be presented with a unsuccessful contact us submission message
 
   Scenario: Invalid Contact Us Form Submission - Using Specific Data
-    Given I navigate to the webdriveruniversity homepage
-    When I click on the contact us button
-    And I type a specific first name "Sarah"
+    When I type a specific first name "Sarah"
     And I type a specific last name "Woods"
     And I enter a specific email address "sarah_woods@example.com"
     And I type specific text "Hello" and number 123 within the comment input field
@@ -30,11 +28,20 @@ Feature: WebdriverUniversity.com - Contact Us Page
     Then I should be presented with a successful contact us submission message
 
   Scenario: Valid Contact Us Form Submission - Using Random Data
-    Given I navigate to the webdriveruniversity homepage
-    When I click on the contact us button
-    And I type a random first name
+    When I type a random first name
     And I type a random last name
     And I enter a random email address
     And I type a comment
     And I click on the submit button
     Then I should be presented with a successful contact us submission message
+
+  Scenario Outline: Validate Contact Us Page
+    When I type a first name <firstName> and a last name <lastName>
+    And I type a email address '<emailAddress>' and a comment '<comment>'
+    And I click on the submit button
+    Then I should be presented with header text '<message>'
+    Examples:
+      | firstName | lastName | emailAddress       | comment           | message                      |
+      | John      | Doe      | john_doe@email.com | hello how are you | Thank You for your Message!  |
+      | Mia       | Carter   | mia12@email.com    | Test 123          | Thank You for your Message!  |
+      | Grace     | Hud      | grace hud112       | Nothing to add    | Error: Invalid email address |
