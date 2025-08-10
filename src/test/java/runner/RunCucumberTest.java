4cc29ef741365c358b23c9375dc0a734967da1bf
Andrei Gunta <andreigunta@yahoo.com>
2025-08-10 13:50:29 +0300

    added surefire plugin

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -21,7 +21,9 @@ import java.util.Properties;
         features = "src/test/resources/features",
         glue = "step_definitions",
         tags = "@smoke and not @ignore",
-        plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-report.html"}
+        plugin = {"pretty", "json:target/cucumber.json"
+//                , "html:target/cucumber-report.html"
+        }
 )
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
